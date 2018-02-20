43ae8b2266517dd4a7bef28dbb2edf92de5d4e43
Akhil Narang <akhilnarang.1999@gmail.com>
2018-02-21 02:01:42 +0530

    Updater: refactor for WrathOS!
      * Remove incremental build version prop stuff, we aren't using that
    
    Change-Id: I6bffd2ad9fc367fc8f17933386011ea006a3afce
    Signed-off-by: Akhil Narang <akhilnarang.1999@gmail.com>
    Signed-off-by: DennySPB <dennyspb@gmail.com>
    Signed-off-by: iamtoxyc <vtechnical1804@gmail.com>

--- src/org/lineageos/updater/misc/Utils.java
+++ src/org/lineageos/updater/misc/Utils.java
@@ -146,7 +146,7 @@ public class Utils {
     }
 
     public static String getServerURL(Context context) {
-        String incrementalVersion = SystemProperties.get(Constants.PROP_BUILD_VERSION_INCREMENTAL);
+        //String incrementalVersion = SystemProperties.get(Constants.PROP_BUILD_VERSION_INCREMENTAL);
         String device = SystemProperties.get(Constants.PROP_NEXT_DEVICE,
                 SystemProperties.get(Constants.PROP_DEVICE));
         String type = SystemProperties.get(Constants.PROP_RELEASE_TYPE).toLowerCase(Locale.ROOT);
@@ -157,8 +157,7 @@ public class Utils {
         }
 
         return serverUrl.replace("{device}", device)
-                .replace("{type}", type)
-                .replace("{incr}", incrementalVersion);
+                .replace("{type}", type);
     }
 
     public static String getChangelogURL(Context context) {
